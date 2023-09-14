9cbeda152bcc5712e5b9552133baab9408c0f7a3
paradoxo <vinicius_baggio@hotmail.com>
2023-09-14 18:16:37 -0300

    Routes/RoutesComponents added

---
+++ src/pages/End.tsx
@@ -0,0 +1,16 @@
+import { IonContent, IonHeader, IonPage, IonTitle } from "@ionic/react";
+
+const Teste: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonTitle>Teste</IonTitle>
+      </IonHeader>
+      <IonContent>
+        <IonTitle>Teste</IonTitle>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Teste;
